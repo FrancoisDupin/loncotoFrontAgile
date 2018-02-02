db4feb4910695419a1209ccbc209e3b6957787c1
Stagiaire <Stagiaire@MININT-DEAA2P1.mshome.net>
2018-02-02 14:56:28 +0100

    test scumb

--- loncoto/src/app/services/article-repository.service.ts
+++ loncoto/src/app/services/article-repository.service.ts
@@ -1,8 +1,48 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
+import { Observable } from 'rxjs/Observable';
+import { BehaviorSubject } from 'rxjs/BehaviorSubject';
+import { Page } from '../metier/Page';
+import { Article } from '../metier/Article';
 
-@Injectable()
+
+Injectable()
 export class ArticleRepositoryService {
 
-  constructor() { }
 
+
+  private articleSubject: BehaviorSubject<Page<Article>>;
+  private searchName: string; //recherche sur le nom
+
+  private baseUrl: string="http://localhost:8080/extendedapi/articles";
+  private apiUrl: string="http://localhost:8080/api/";
+  private numPage:number;
+
+  constructor(private _http:HttpClient) {
+
+    this.searchName="";
+    this.articleSubject= new BehaviorSubject(new Page([],0,0,5,0,1,true,false,null));
+    this.numPage=0;
+
+   }
+
+   public listeArticlesObservable(): Observable<Page<Article>>{
+     return this.articleSubject.asObservable();
+   }
+
+
+  refreshListe(): any {
+    let url= `${this.baseUrl}/articles`;
+    if(this.searchName!="") {
+      url+=`/search/${this.searchName}`;
+    }
+    let params : HttpParams= new HttpParams();
+    params= params.set("page",""+this.numPage);
+
+
+    this._http.get<Page<Article>>(url,{params: params})
+              .toPromise()
+              .then(articles=>this.articleSubject.next(articles));
+
+  }
 }
