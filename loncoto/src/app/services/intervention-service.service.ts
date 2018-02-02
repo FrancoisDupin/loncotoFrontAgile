a203f5e60e71e68c1892a37fc3dcb3500f97d908
FrancoisDupin <francoisnoel.dupin@gmail.com>
2018-02-02 11:10:36 +0100

    micro commit, hum.

--- loncoto/src/app/services/intervention-service.service.ts
+++ loncoto/src/app/services/intervention-service.service.ts
@@ -3,7 +3,7 @@ import { Injectable } from '@angular/core';
 @Injectable()
 export class InterventionServiceService {
 
-  private baseurl:string = "localhost:8080/"; // TODO => completer
+  private baseurl:string = "http://localhost:8080/"; // TODO => completer
 
   constructor() { }
 
